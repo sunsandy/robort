a7e8fc083635ddd7f95a97c30cfed932edb432bb
Beeeeeeeee <2645546093@qq.com>
2017-08-28 21:17:34 +0800

    light_change
    对light做了注释

--- Robort_Stm32/APP/App_Light.c
+++ Robort_Stm32/APP/App_Light.c
@@ -1,5 +1,37 @@
+/**********************************************************************************************************
+**** 文件名：APP_light.c
+****
+**** 文件描述：控制两盏灯的状态（亮/灭）
+****
+**** 创建人：
+****
+**** 版本号：1.1
+****
+**** 修改记录：2017/8/28  By SunKang
+****
+****
+****
+***********************************************************************************************************/
+
 #include "App_Light.h"
 
+
+/*************************************************************************************************
+***** 函数名：void Robort_Light_Control(ROBORT_LIGHT_NO_TAG light_no, ROBORT_LIGHT_TAG light_status)
+*****
+***** 入口参数：light_no：选择控制灯1或灯2，ROBORT_LIGHT_ONE = 0，ROBORT_LIGHT_TWO = 1
+*****                  light_status:选择被控制灯的状态，ROBORT_LIGHT_ON = 0，ROBORT_LIGHT_OFF=1
+*****
+***** 功能描述：控制两个灯的点亮和熄灭
+*****						light_no =	ROBORT_LIGHT_ONE 时，控制灯1的状态
+*****						light_no =	ROBORT_LIGHT_TWO 时，控制灯2的状态
+*****						light_status = ROBORT_LIGHT_ON 时，对应的灯被点亮
+*****						light_status = ROBORT_LIGHT_OFF 时，对应的灯熄灭
+*****
+***** 返回值：无
+*****
+***** 作者：
+**************************************************************************************************/
 
 void Robort_Light_Control(ROBORT_LIGHT_NO_TAG light_no, ROBORT_LIGHT_TAG light_status)
 {
@@ -9,20 +41,34 @@ void Robort_Light_Control(ROBORT_LIGHT_NO_TAG light_no, ROBORT_LIGHT_TAG light_s
         case ROBORT_LIGHT_ONE:
               Control_1stLight(light_status);
         break;
-        
+
         case ROBORT_LIGHT_TWO:
               Control_2ndLight(light_status);
         break;
-        
+
         default: break;
     }
-    
+
 }
 
+/*************************************************************************************************
+***** 函数名：void Control_1stLight(ROBORT_LIGHT_TAG light_status)
+*****					void Control_2ndLight(ROBORT_LIGHT_TAG light_status)
+*****
+***** 入口参数：light_status：
+*****
+***** 功能描述：控制两个灯的点亮和熄灭（为函数Robort_Light_Control()的所调用）
+*****						当入口参数为 ROBORT_LIGHT_ON （ROBORT_LIGHT_ON = 0）时，对应的灯点亮
+*****						当入口参数为 ROBORT_LIGHT_OFF ROBORT_LIGHT_OFF = 1）时，对应的灯熄灭
+*****
+***** 返回值：无
+*****
+***** 作者：
+**************************************************************************************************/
 static void Control_1stLight(ROBORT_LIGHT_TAG light_status)
 {
-    (ROBORT_LIGHT_ON==light_status) ? 
-        STM32_GPIO_SetPin(GPIOA, GPIO_Pin_11, 1) : STM32_GPIO_SetPin(GPIOA, GPIO_Pin_11, 0); 
+    (ROBORT_LIGHT_ON==light_status) ?
+        STM32_GPIO_SetPin(GPIOA, GPIO_Pin_11, 1) : STM32_GPIO_SetPin(GPIOA, GPIO_Pin_11, 0);
 }
 
 static void Control_2ndLight(ROBORT_LIGHT_TAG light_status)
@@ -30,16 +76,27 @@ static void Control_2ndLight(ROBORT_LIGHT_TAG light_status)
 
 }
 
- void Robort_Light_Init(void)
-{
-    GPIO_InitTypeDef GPIO_InitStructure;  
- 
-    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE); 			
-  	GPIO_InitStructure.GPIO_Pin   = GPIO_Pin_11;        //GPIOA_Pin11 -- LIGHT
-  	GPIO_InitStructure.GPIO_Mode  = GPIO_Mode_Out_PP;			
-  	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-  	GPIO_Init(GPIOA, &GPIO_InitStructure);  
-    
-    STM32_GPIO_SetPin(GPIOA, GPIO_Pin_11, 0); 
-}
 
+/*************************************************************************************************
+***** 函数名：void Robort_Light_Init(void)
+*****
+***** 入口参数：无
+*****
+***** 功能描述：初始化Lihgt相关的GPIO，初始状态时灯为熄灭状态
+*****
+***** 返回值：无
+*****
+***** 作者：
+**************************************************************************************************/
+void Robort_Light_Init(void)
+{
+    GPIO_InitTypeDef GPIO_InitStructure;
+
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE); //使能GPIOA端口时钟
+  	GPIO_InitStructure.GPIO_Pin   = GPIO_Pin_11;          //GPIOA_Pin11 -- LIGHT
+  	GPIO_InitStructure.GPIO_Mode  = GPIO_Mode_Out_PP;     //推挽输出
+  	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;     //IO口速度为50MHz
+  	GPIO_Init(GPIOA, &GPIO_InitStructure);                //推挽输出 ，IO口速度为50MHz
+
+    STM32_GPIO_SetPin(GPIOA, GPIO_Pin_11, 0);             //GPIOA_Pin11设置为低
+}
